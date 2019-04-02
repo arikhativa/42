405b378814c45bbff7d5f9869d192f66d78be35d
yoav rabby <yoavar@protonmail.com>
2019-04-02 16:24:58 +0200

    first steps in libft

---
+++ libft/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrabby <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/02 16:15:43 by yrabby            #+#    #+#             */
+/*   Updated: 2019/04/02 16:15:45 by yrabby           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef LIBFT_A
+# define LIBFT_A
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int		ft_strlen(char *str);
+char	*ft_strdup(char *str);
+
+#endif
---
+++ libft/main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
+
+
+int main()
+{
+	char s1[] = "hey";
+	char *s2;
+
+	s2 = ft_strdup(s1);
+	printf("%s\n", s2);
+	return (0);
+}
