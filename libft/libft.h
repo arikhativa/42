0940723d806b671918ab0eebd6c1ce32ba885a75
yoav rabby <yoavar@protonmail.com>
2019-04-24 14:22:52 +0200

    fffff42

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yrabby <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/10 14:56:51 by yrabby            #+#    #+#             */
-/*   Updated: 2019/04/21 15:03:55 by yrabby           ###   ########.fr       */
+/*   Updated: 2019/04/24 14:14:22 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ char			*ft_strchr(const char *str, int n);
 char			*ft_strrchr(const char *str, int n);
 char			*ft_strstr(const char *s1, const char *s2);
 char			*ft_strnstr(const char *s1, const char *s2, size_t n);
-char			*nstr(const char *s1, const char *s2, size_t n);
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
 int				ft_isascii(int c);
@@ -44,7 +43,7 @@ char			*ft_strcat(char *dst, const char *src);
 char			*ft_strncat(char *dst, const char *src, size_t i);
 size_t			ft_strlcat(char *dst, const char *src, size_t i);
 void			*ft_memset(void *ptr, int n, size_t len);
-int				ft_memcmp(void *s1, void *s2, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
 void			*ft_memchr(const void *str, int c, size_t n);
 int				ft_toupper(int n);
 int				ft_tolower(int n);
